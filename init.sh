163d1babc8c48a8734b62b27df5d84c7b6fd7f0d
ainur270707 <103682607+ainur270707@users.noreply.github.com>
2024-10-05 12:54:16 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Установка системных зависимостей
-apt-get update && apt-get install -r requirements.txt
-Flask
-opencv-contrib-python-headless
-ultralytics
-libc6
+apt-get update && apt-get install -y libc6  # Установка системных пакетов
+
+# Установка Python-зависимостей
+pip install -r requirements.txt  # Установка зависимостей из requirements.txt
+
 # Запуск приложения
-python3 app.py
+python3 app.py  # Запуск вашего приложения
