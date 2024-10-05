e2949431a89a0f99705b4ef8f001e02c37df64a8
ainur270707 <103682607+ainur270707@users.noreply.github.com>
2024-10-05 12:18:12 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,6 +2,9 @@
 
 # Установка системных зависимостей
 apt-get update && apt-get install -y libgl1-mesa-glx
-
+Flask
+opencv-contrib-python-headless
+ultralytics
+libc6
 # Запуск приложения
 python3 app.py
