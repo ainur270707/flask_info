cc550ded0ac62b3366d72e213fb023add71bca58
ainur270707 <103682607+ainur270707@users.noreply.github.com>
2024-10-06 15:04:12 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,2 @@
-#!/bin/bash
 
-set -e  # Завершить скрипт при ошибке
-
-echo "Начало выполнения init.sh"
-
-echo "Активация виртуального окружения..."
-source /opt/venv/bin/activate
-
-echo "Установка Python-зависимостей из requirements.txt..."
 pip install --no-cache-dir -r requirements.txt
-
-echo "init.sh выполнен успешно."
